f74b1616d2ffb9bd540c38a6736e126c69b79db5
Alonso A. Ortega <alayor3@gmail.com>
2018-12-29 13:43:27 -0700

    Add input box with mininum buying quantity.

--- src/ui/2_quantity/index.js
+++ src/ui/2_quantity/index.js
@@ -5,11 +5,15 @@ import React from "react";
 export class QuantityPage extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      quantity: this.props.params.product.minimumBuyingQuantity
+    }
   }
 
   render() {
-    return <div>Hello {this.props.params.product.name}</div>
+    return <div>
+      <input value={this.state.quantity}/> {this.props.params.product.quantityType} de {this.props.params.product.name}
+    </div>
   }
 
 }
