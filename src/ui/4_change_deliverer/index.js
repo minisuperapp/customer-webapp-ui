e2be4cc45d82639ad6612f5119c830e443d6f132
Alonso A. Ortega <alayor3@gmail.com>
2018-12-30 10:45:39 -0700

    Show deliverers.

--- src/ui/4_change_deliverer/index.js
+++ src/ui/4_change_deliverer/index.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React from 'react'
 import { views } from 'src/ui/Views'
+import * as offersService from 'src/services/offers'
+import { ChangeDeliverer } from './ChangeDeliverer'
 
 export class ChangeDelivererView extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      offers: [],
+    }
+  }
+
+  async componentDidMount() {
+    const serviceResponse = await offersService.getOffers(
+      this.props.customerLocation,
+      this.props.params.productCode,
+      this.props.params.quantity,
+    )
+    this.setState({ offers: serviceResponse.data })
   }
 
   render() {
-    return <div>
-      Buscando repartidores...
-    </div>
+    return <ChangeDeliverer {...this.state}/>
   }
-
 }
