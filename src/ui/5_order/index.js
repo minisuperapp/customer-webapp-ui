5bdc442ce429f96f24c1b1e13ea6bd79debf2ef1
Alonso A. Ortega <alayor3@gmail.com>
2018-12-30 19:09:55 -0700

    Add order view.

---
+++ src/ui/5_order/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { views } from 'src/ui/Views'
+
+export class OrderView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>
+      Orden enviada!
+    </div>
+  }
+
+}
--- src/ui/Views.js
+++ src/ui/Views.js
@@ -2,10 +2,12 @@ import { ProductsView } from "./1_products";
 import { QuantityView } from "./2_quantity";
 import { AssignedOfferView } from "./3_assigned_offer";
 import { ChangeDelivererView } from "./4_change_deliverer";
+import { OrderView } from "./5_order";
 
 export const views = {
   PRODUCTS: ProductsView,
   QUANTITY: QuantityView,
   ASSIGNED_OFFER: AssignedOfferView,
   CHANGE_DELIVERER: ChangeDelivererView,
+  ORDER: OrderView,
 }
