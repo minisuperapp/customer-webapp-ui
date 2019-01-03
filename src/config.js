92322818d655f5db7bf50dc20f3d598db3de1b67
Alonso A. Ortega <alayor3@gmail.com>
2019-01-02 22:03:08 -0700

    Fix endpoint.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // API_HOST: 'http://ec2-52-53-161-6.us-west-1.compute.amazonaws.com:3000',
-  API_HOST: 'http://localhost:3000',
+  API_HOST: 'http://ec2-52-53-161-6.us-west-1.compute.amazonaws.com:3000',
+  // API_HOST: 'http://localhost:3000',
   get API_URL() {
     return `${this.API_HOST}/api`
   },
