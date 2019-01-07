ab80754d178f40d782eef7090600326a7e4fb11f
Alonso A. Ortega <alayor3@gmail.com>
2019-01-07 12:40:40 -0700

    Reformat.

--- src/ui/views/1_products/Products/Product/index.js
+++ src/ui/views/1_products/Products/Product/index.js
@@ -1,48 +1,53 @@
-import React from "react";
+import React from 'react'
 import { views } from 'src/ui/views/index'
-import {styles} from "./styles";
+import { styles } from './styles'
 
 export class Product extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   render() {
- const productImages = {
- ORANGE: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/orange_available.jpg')`,
- RED_APPLE: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/red_apple_available.jpg')`,
- CORN_TORTILLA: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/corn_tortilla_available.jpg')`
-}
- styles.image={
-   display: "flex",
-   justifyContent:"center",
-   backgroundImage: productImages[this.props.product.code],
-   backgroundSize: 'contain',
-   backgroundRepeat: 'no-repeat',
-   height: "100px",
-  width: "100px"
- }
+    const productImages = {
+      ORANGE: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/orange_available.jpg')`,
+      RED_APPLE: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/red_apple_available.jpg')`,
+      CORN_TORTILLA: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/corn_tortilla_available.jpg')`,
+    }
+    styles.image = {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundImage: productImages[this.props.product.code],
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      height: '100px',
+      width: '100px',
+    }
 
     return (
-        <div style={styles.container}>
-        <div style = {styles.image}>
-        </div>
-        <div style = {styles.mainPanel}>
-        <div style = {styles.textTitle}>  {this.props.product.name}</div>
-        <div style = {styles.textPanel}>
-        <div style = {styles.textBody}>${this.props.lowestPrice}</div>
-        <div style = {styles.textBody}>  {this.props.lowestPrice && (
-                <button style={styles.button}  onClick={() => this.props.changeView(views.QUANTITY, {
-            product: this.props.product,
-            lowestPrice: this.props.lowestPrice
-          })}>
+      <div style={styles.container}>
+        <div style={styles.image} />
+        <div style={styles.mainPanel}>
+          <div style={styles.textTitle}> {this.props.product.name}</div>
+          <div style={styles.textPanel}>
+            <div style={styles.textBody}>${this.props.lowestPrice}</div>
+            <div style={styles.textBody}>
+              {' '}
+              {this.props.lowestPrice && (
+                <button
+                  style={styles.button}
+                  onClick={() =>
+                    this.props.changeView(views.QUANTITY, {
+                      product: this.props.product,
+                      lowestPrice: this.props.lowestPrice,
+                    })
+                  }>
                   <div style={styles.buttonText}>Comprar</div>
                 </button>
-          )}
-          </div>
-          </div>
+              )}
+            </div>
           </div>
         </div>
-    );
+      </div>
+    )
   }
 }
