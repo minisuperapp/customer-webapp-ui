7f0fb5e83bbad858d67aa867eff30dc97739b9fb
Alonso A. Ortega <alayor3@gmail.com>
2019-01-13 21:45:32 -0700

    Change api.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_HOST: 'https://minisuper.app/customer-api',
+  API_HOST: 'https://minisuper.app:3300',
   // API_HOST: 'http://localhost:3000',
   get API_URL() {
     return `${this.API_HOST}/api`
