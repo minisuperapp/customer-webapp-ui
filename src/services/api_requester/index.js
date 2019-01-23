92b92375a1e57e18580444686b6b26508fa9f1d4
Alonso A. Ortega <alayor3@gmail.com>
2019-01-23 14:02:08 -0700

    Fix unexpected token.

--- src/services/api_requester/index.js
+++ src/services/api_requester/index.js
@@ -3,32 +3,35 @@ import config from 'src/config'
 
 export const send = async (request) => {
   let headers = {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   }
   if (isTestEnv()) {
-  headers = {
-    ...headers,
-    'is-test': 'true'
-  }
-  const info = {
-    method: request.method,
-    url: config.API_URL + '/' + request.path,
-    data: JSON.stringify(request.payload),
-    withCredentials: true,
-    headers
-  }
-  console.log('request: ', info)
-  try {
-    const response = await axios(info)
-    console.log('response: ', response)
-    return response
-  } catch (err) {
-    console.log(request.uri + '/' + request.path, info)
-    console.log(err)
+    headers = {
+      ...headers,
+      'is-test': 'true',
+    }
+    const info = {
+      method: request.method,
+      url: config.API_URL + '/' + request.path,
+      data: JSON.stringify(request.payload),
+      withCredentials: true,
+      headers,
+    }
+    console.log('request: ', info)
+    try {
+      const response = await axios(info)
+      console.log('response: ', response)
+      return response
+    } catch (err) {
+      console.log(request.uri + '/' + request.path, info)
+      console.log(err)
+    }
   }
 }
 
 function isTestEnv() {
-  return !(/^https:\/\/www\.minisuper\.app\/sell/.test(window.location.href) ||
-    /^https:\/\/minisuper\.app\/sell/.test(window.location.href))
+  return !(
+    /^https:\/\/www\.minisuper\.app\/sell/.test(window.location.href) ||
+    /^https:\/\/minisuper\.app\/sell/.test(window.location.href)
+  )
 }
