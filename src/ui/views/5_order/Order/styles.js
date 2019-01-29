a76021de4cf6791e3d0821b805732a8f006b82f5
Bryan Hernandez <bryanh24@hotmail.com>
2019-01-29 17:24:05 -0600

    improved view

---
+++ src/ui/views/5_order/Order/styles.js
@@ -0,0 +1,37 @@
+export const styles = {
+  container: {
+    marginTop: '5vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  textTilte: {
+    marginTop: '3vh',
+    display: 'flex',
+    marginLeft: '5px',
+    fontFamily: 'SFProText-Bold',
+    fontSize: '3.5vh',
+    color: '#212121',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  backButton: {
+    backgroundColor: '#a24d0d',
+    borderRadius: '18px',
+    boxShadow: '0 2px 8px 0 rgba(127, 31, 26, 1)',
+    border: 'none',
+    color: 'white',
+    width: '20%',
+    textAlign: 'center',
+    marginTop: '5vh',
+    height: '5%',
+    fontSize: '3vh',
+  },
+}
