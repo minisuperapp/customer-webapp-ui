b24cfb5fbf6c8f82f3f19af7abee1d90662522e0
YojanR <ydrv.148@gmail.com>
2019-02-04 20:27:28 -0700

    removing duplicated code

--- src/ui/views/5_order/Order/index.js
+++ src/ui/views/5_order/Order/index.js
@@ -18,20 +18,5 @@ export class Order extends React.Component {
           Volver a productos
         </button>
     </div>
-    return (
-      <div style={styles.container}>
-        <div style={styles.textTilte}>
-          {this.props.status === 'STARTED' && 'Tu pedido se ha enviado.'}
-          {this.props.status === 'IN_TRANSIT' && 'Tu pedido esta en camino!'}
-        </div>
-        <div style={styles.textTilte}>Tu repartidor: {this.props.params.offer.deliverer.name}</div>
-        <div style={styles.textTilte}>
-          Tu orden: {this.props.params.order.productQuantity}{' '}
-          {this.props.params.product.quantityType} de {this.props.params.product.name}
-        </div>
-        <div style={styles.textTilte}>Total: ${this.props.params.order.total}</div>
-        <button style={styles.backButton}>Volver</button>
-      </div>
-    )
   }
 }
