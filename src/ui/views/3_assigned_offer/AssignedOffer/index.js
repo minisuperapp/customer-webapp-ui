ce98db1df469833ffa65549e362089146c612887
Alonso A. Ortega <alayor3@gmail.com>
2019-02-23 13:52:55 -0700

    Use css modules in assgined offer view style.

--- src/ui/views/3_assigned_offer/AssignedOffer/index.js
+++ src/ui/views/3_assigned_offer/AssignedOffer/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import {index} from 'src/ui/views/index'
-import { views } from 'src/ui/views/index'
-import { styles } from './styles'
+import React from 'react'
+import css from './styles.module.css'
 
 export class AssignedOffer extends React.Component {
   render() {
@@ -10,47 +8,45 @@ export class AssignedOffer extends React.Component {
       RED_APPLE: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/red_apple_available.jpg')`,
       CORN_TORTILLA: `url('https://s3.us-west-1.amazonaws.com/minisuper.images/corn_tortilla_available.jpg')`,
     }
-    styles.image = {
-      display: 'flex',
-      marginTop: '40px',
-      justifyContent: 'center',
-      alignItem: 'center',
+    const style = {
       backgroundImage: productImages[this.props.params.product.code],
-      backgroundSize: 'contain',
-      backgroundRepeat: 'no-repeat',
-      height: '100px',
-      width: '100px',
     }
-	
+
     return (
-      <div style={styles.container}>
-	    <div style={styles.textTitle}>Revise su orden</div>
-        <div style={styles.image}></div>
-        <div style={styles.space}>
-          <div style={styles.normalText}>Producto: </div>
-          <div style={styles.priceText}>{this.props.params.product.name}</div>
+      <div className={css.container}>
+        <div className={css.textTitle}>Revise su orden</div>
+        <div className={css.image} style={style}/>
+        <div className={css.space}>
+          <div className={css.normalText}>Producto:</div>
+          <div className={css.priceText}>{this.props.params.product.name}</div>
         </div>
-        <div style={styles.space}>
-          <div style={styles.normalText}>Cantidad: </div>
-          <div style={styles.priceText}>{this.props.params.quantity}</div>
+        <div className={css.space}>
+          <div className={css.normalText}>Cantidad:</div>
+          <div className={css.priceText}>{this.props.params.quantity}</div>
         </div>
-        <div style={styles.space}>
-          <div style={styles.normalText}>Precio: </div>
-          <div style={styles.priceText}>${this.props.offer.unitPrice}</div>
+        <div className={css.space}>
+          <div className={css.normalText}>Precio:</div>
+          <div className={css.priceText}>${this.props.offer.unitPrice}</div>
         </div>
-        <div style={styles.space}>
-          <div style={styles.normalText}>Total: </div>
-          <div style={styles.priceText}>${this.props.total}</div>
+        <div className={css.space}>
+          <div className={css.normalText}>Total:</div>
+          <div className={css.priceText}>${this.props.total}</div>
         </div>
-        <div style={styles.space}>
-          <div style={styles.normalText}>Proveedor: </div>
-          <div style={styles.priceText}>{this.props.offer.deliverer.name}</div>
+        <div className={css.space}>
+          <div className={css.normalText}>Proveedor:</div>
+          <div className={css.priceText}>{this.props.offer.deliverer.name}</div>
         </div>
-        <button style={styles.button} onClick={this.props.changeDeliverer}>Cambiar proveedor</button>
-        <p></p>
-        <button style={styles.button} onClick={this.props.order}>Pedir</button>
-        <button style={styles.backButton} onClick={this.props.onCancel}>Volver</button>
+        <button className={css.button} onClick={this.props.changeDeliverer}>
+          Cambiar proveedor
+        </button>
+        <p />
+        <button className={css.button} onClick={this.props.order}>
+          Pedir
+        </button>
+        <button className={css.backButton} onClick={this.props.onCancel}>
+          Volver
+        </button>
       </div>
-    );
+    )
   }
 }
