93e80c3942cdecc641cd918f483d781fdaa15b30
alayor <alayor3@gmail.com>
2020-03-31 06:53:04 -0700

    Change homepage

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   get API_HOST() {
-    return 'http://localhost:3000'
+    return 'https://minisuper-customer-api.herokuapp.com'
   },
   get API_URL() {
     return `${this.API_HOST}/api`
@@ -8,13 +8,15 @@ module.exports = {
   get isTestEnv() {
     return !(
       /^https:\/\/www\.minisuper\.app\/buy/.test(window.location.href) ||
-      /^https:\/\/minisuper\.app\/buy/.test(window.location.href)
+      /^https:\/\/minisuper\.app\/buy/.test(window.location.href) ||
+      /^https:\/\/minisuper-customer-webapp-ui\.herokuapp\.com/.test(window.location.href)
     )
   },
   get isApiHostProd() {
     return (
       /^https:\/\/www\.minisuper\.app\/buy/.test(this.API_HOST) ||
-      /^https:\/\/minisuper\.app\/buy/.test(this.API_HOST)
+      /^https:\/\/minisuper\.app\/buy/.test(this.API_HOST) ||
+      /^https:\/\/minisuper-customer-api\.herokuapp\.com/.test(this.API_HOST)
     )
   },
   get socketPayload() {
