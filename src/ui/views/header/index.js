7cc07a457b18b2cb78904ed80e37720c1051b891
Raul T <jr.torsa@gmail.com>
2020-07-07 17:01:29 -0600

    making 'Crear una cuenta' clickable

--- src/ui/views/header/index.js
+++ src/ui/views/header/index.js
@@ -5,7 +5,9 @@ import css from './styles.module.css'
 const Index = () => (
   <div className={css.container}>
     <img src={logo} width="114" height="50" align="absmiddle" />
-    <div className={css.account}> Crear una Cuenta </div>
+    <div className={css.account}>
+      <a href="">Crear una Cuenta</a>
+    </div>
   </div>
 )
 
