ad1f4124ddc78e091440cb3e920115735cf99713
Raul T <jr.torsa@gmail.com>
2020-07-07 14:12:41 -0600

    adding styles to register form

--- src/ui/views/register/index.js
+++ src/ui/views/register/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import css from './styles.module.css'
 
 export default class Register extends Component {
   render() {
     return (
       <>
-        <div>Ingresa tus datos de registro</div>
+        <div className={css.header}>Ingresa tus datos de registro</div>
         <div>
-          <label>Nombre: </label>
-          <input type="text" />
+          <label htmlFor="name">Nombre: </label>
+          <input name="name" type="text" placeholder="Nombre" />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input name="email" type="text" placeholder="Email" />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña: </label>
+          <input name="password" type="password" placeholder="Contraseña" />
         </div>
       </>
     )
