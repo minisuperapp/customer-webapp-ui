b63fb9f1c43d068380955424a0a6b86da5e7aa5d
Raul T <jr.torsa@gmail.com>
2020-07-10 12:46:59 -0600

    changed css module => style.js in change deliverer/deliverer/index.js

--- src/ui/views/change_deliverer/ChangeDeliverer/index.js
+++ src/ui/views/change_deliverer/ChangeDeliverer/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import css from './styles.module.css'
+import Style from './style'
 import Deliverer from './Deliverer'
 
 export class ChangeDeliverer extends React.Component {
   render() {
     return (
-      <div>
+      <Style>
         {this.props.offers.length
           ? this.props.offers.map((o) => <Deliverer offer={o} key={o.id} />)
           : 'Buscando repartidores...'}
-        <button className={css.backButton} onClick={this.props.goToAssignedOffer}>
+        <button className="backButton" onClick={this.props.goToAssignedOffer}>
           Volver a Revisión de Orden
         </button>
-      </div>
+      </Style>
     )
   }
 }
