3db930bdcef89253b62756b81558f865675075a7
Raul T <jr.torsa@gmail.com>
2020-07-13 19:19:48 -0600

    changed styles.modules.css => style.js in products/components/loadinglistindex.js

--- src/ui/views/products/components/LoadingList/index.js
+++ src/ui/views/products/components/LoadingList/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Item } from './Item'
-import css from './styles.module.css'
+import Style from './style'
 
 export class LoadingList extends React.Component {
   render() {
     return (
-      <div>
-        <div className={css.container}>
+      <Style>
+        <div className="container">
           <Item />
           <Item />
           <Item />
         </div>
-      </div>
+      </Style>
     )
   }
 }
