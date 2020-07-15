41b26c5f05ed617c8de955fb418d0853afe6f35b
alayor <alayor3@gmail.com>
2020-07-14 22:59:52 -0600

    Use paths in Link.

--- src/ui/views/header/index.js
+++ src/ui/views/header/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from 'src/ui/images/logo.png'
 import Style from './style'
+import { paths } from 'src/constants'
 
 const Index = () => (
   <Style>
     <img src={logo} width="114" height="50" align="absmiddle" />
     <div className="account">
-      <Link to="/register">Crear una Cuenta</Link>
+      <Link to={paths.register}>Crear una Cuenta</Link>
     </div>
   </Style>
 )
