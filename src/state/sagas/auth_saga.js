81f6c3acfa754d5458c316c68bb7cbb2b51a77ab
alayor <alayor3@gmail.com>
2020-07-17 10:56:21 -0600

    Call the register customer service on the saga.

--- src/state/sagas/auth_saga.js
+++ src/state/sagas/auth_saga.js
@@ -1,9 +1,11 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
 import * as types from 'src/state/actions/action_types'
+import { register_customer_response } from '../actions/auth_actions'
+import * as auth_service from 'src/state/services/auth'
 
 export function* register_customer() {
   yield takeEvery(types.REGISTER_CUSTOMER_REQUEST, function* () {
-    const response = yield call(register_customer)
-    yield put(register_customer(response))
+    const response = yield call(auth_service.register_customer)
+    yield put(register_customer_response(response))
   })
 }
