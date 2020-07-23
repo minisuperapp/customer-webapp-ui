c2c20ed2c7ca3f6419794d5403fc066970e02384
alayor <alayor3@gmail.com>
2020-07-23 11:24:13 -0600

    Hard code location for now.

--- src/state/services/location/index.js
+++ src/state/services/location/index.js
@@ -1,15 +1,19 @@
 export const get_location = async () => {
-  try {
-    const location = await get_position()
-    return {
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-    }
-  } catch (e) {
-    return {
-      latitude: '28.1874198',
-      longitude: '-105.4671407',
-    }
+  // try {
+  //   const location = await get_position()
+  //   return {
+  //     latitude: location.coords.latitude,
+  //     longitude: location.coords.longitude,
+  //   }
+  // } catch (e) {
+  //   return {
+  //     latitude: '28.1874198',
+  //     longitude: '-105.4671407',
+  //   }
+  // }
+  return {
+    latitude: '28.1874198',
+    longitude: '-105.4671407',
   }
 }
 
