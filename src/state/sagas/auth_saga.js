3083c733f34d96acdbf8a1c9450b13d4a6cb0a53
Raul T <jr.torsa@gmail.com>
2020-07-23 21:21:15 -0600

    added function to sagas

--- src/state/sagas/auth_saga.js
+++ src/state/sagas/auth_saga.js
@@ -1,6 +1,6 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
 import * as types from 'src/state/actions/action_types'
-import { register_customer_response } from '../actions/auth_actions'
+import { login_customer_response, register_customer_response } from '../actions/auth_actions'
 import * as auth_service from 'src/state/services/auth'
 
 export function* register_customer() {
@@ -12,5 +12,12 @@ export function* register_customer() {
       }
     }
     yield put(register_customer_response(response))
+  })
+}
+
+export function* login_customer() {
+  yield takeEvery(types.LOGIN_CUSTOMER_REQUEST, function* () {
+    const response = yield call(auth_service.login_customer)
+    yield put(login_customer_response(response))
   })
 }
