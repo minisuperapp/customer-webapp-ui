92b5d24019244cb84687d87b46b52bf298bf690f
Raul T <jr.torsa@gmail.com>
2020-07-24 22:08:15 -0600

    adding form

--- src/ui/views/delivery_address/components/DeliveryAddressForm/index.js
+++ src/ui/views/delivery_address/components/DeliveryAddressForm/index.js
@@ -3,11 +3,13 @@ import Style from './style'
 
 export class DeliveryAddressForm extends React.Component {
   render() {
-
     return (
       <Style>
         <div className="title">Ingresa el lugar de entrega</div>
-
+        <div>
+          <label htmlFor="name">Nombre</label>
+          <input name="name" placeholder="A donde te entregamos?" type="text" />
+        </div>
       </Style>
     )
   }
