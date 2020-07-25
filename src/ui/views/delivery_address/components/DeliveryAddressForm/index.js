5603e14f0da639bbd6f940b7842b80b28d378fea
Raul T <jr.torsa@gmail.com>
2020-07-24 22:44:31 -0600

    adding styles to form

--- src/ui/views/delivery_address/components/DeliveryAddressForm/index.js
+++ src/ui/views/delivery_address/components/DeliveryAddressForm/index.js
@@ -6,41 +6,44 @@ export class DeliveryAddressForm extends React.Component {
     return (
       <Style>
         <div className="title">Ingresa el lugar de entrega</div>
-        <div>
-          <label htmlFor="name">Nombre</label>
-          <input name="name" placeholder="A donde te entregamos?" type="text" />
+        <div className="body">
+          <div>
+            <label htmlFor="name">Nombre</label>
+            <input name="name" placeholder="A donde te entregamos?" type="text" />
+          </div>
+          <div>
+            <label htmlFor="street">Calle</label>
+            <input name="street" placeholder="Calle" type="text" />
+          </div>
+          <div>
+            <label htmlFor="exterior-number">Numero Exterior</label>
+            <input name="exterior-number" placeholder="Numero Exterior" type="number" />
+          </div>
+          <div>
+            <label htmlFor="interior-number">Numero Interior</label>
+            <input name="interior-number" placeholder="Numero Interior" type="number" />
+          </div>
+          <div>
+            <label htmlFor="neighboorhood">Colonia</label>
+            <input name="neighboorhood" placeholder="Colonia" type="text" />
+          </div>
+          <div>
+            <label htmlFor="city">Ciudad</label>
+            <input name="city" placeholder="Ciudad" type="text" />
+          </div>
+          <div>
+            <label htmlFor="zip-code">Codigo Postal</label>
+            <input name="zip-code" placeholder="Codigo Postal" type="number" />
+          </div>
         </div>
-        <div>
-          <label htmlFor="street">Calle</label>
-          <input name="street" placeholder="Calle" type="text" />
-        </div>
-        <div>
-          <label htmlFor="exterior-number">Numero Exterior</label>
-          <input name="exterior-number" placeholder="Numero Exterior" type="number" />
-        </div>
-        <div>
-          <label htmlFor="interior-number">Numero Interior</label>
-          <input name="interior-number" placeholder="Numero Interior" type="number" />
-        </div>
-        <div>
-          <label htmlFor="neighboorhood">Colonia</label>
-          <input name="neighboorhood" placeholder="Colonia" type="text" />
-        </div>
-        <div>
-          <label htmlFor="city">Ciudad</label>
-          <input name="city" placeholder="Ciudad" type="text" />
-        </div>
-        <div>
-          <label htmlFor="zip-code">Codigo Postal</label>
-          <input name="zip-code" placeholder="Codigo Postal" type="number" />
-        </div>
+
         <div>
           <label htmlFor="state">Estado</label>
           <input name="state" placeholder="Estado" type="text" />
         </div>
         <div className="buttons">
-          <button>Verifica tu Pedido</button>
-          <button>Añade esta Direccion</button>
+          <button className="verify">Verifica tu Pedido</button>
+          <button className="add">Añade esta Direccion</button>
         </div>
       </Style>
     )
--- src/ui/views/delivery_address/components/DeliveryAddressForm/style.js
+++ src/ui/views/delivery_address/components/DeliveryAddressForm/style.js
@@ -6,12 +6,37 @@ export default styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-family: Roboto, Helvetica, sans-serif;
 
   .title {
-    font-family: SFProText-Bold, serif;
     font-size: 24px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-color: #6f8f72;
+    padding: 0.5em 1em;
+    text-align: center;
+    color: whitesmoke;
+    font-weight: bold;
+  }
+
+  .body {
+    margin-top: 20px;
+    font-size: 18px;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+
+    .verify {
+      font-size: 16px;
+      padding: 10px;
+    }
+
+    .add {
+      font-size: 16px;
+    }
   }
 `
