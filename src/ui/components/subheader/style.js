22fce2a6200ffb557b0c615b64c3893fe935be09
alayor <alayor3@gmail.com>
2020-08-05 23:14:57 -0600

    Add height to the subheader.

--- src/ui/components/subheader/style.js
+++ src/ui/components/subheader/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export default styled.div`
   display: flex;
   align-items: center;
+  height: 3em;
   justify-content: center;
   border-bottom: 1px solid grey;
 
@@ -10,7 +11,7 @@ export default styled.div`
     margin-top: 1em;
     margin-bottom: 1em;
     padding: 0.2em 2em;
-    
+
     & a {
       text-decoration: none;
       color: black;
@@ -30,16 +31,16 @@ export default styled.div`
       border-bottom-right-radius: 10px;
       padding: 0.5em;
     }
-    
+
     & .account_link {
       border: 1px solid black;
       border-top-right-radius: 10px;
       border-bottom-right-radius: 10px;
       padding: 0.5em;
     }
-    
+
     & .active {
-      background-color: rgb(73,162,91);
+      background-color: rgb(73, 162, 91);
       color: white;
     }
   }
