fc635e12f0bf89f51da6713961867b6fa257e58c
alayor <alayor3@gmail.com>
2020-08-05 23:11:53 -0600

    Add height to the header.

--- src/ui/components/header/style.js
+++ src/ui/components/header/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export default styled.div`
   display: flex;
   justify-content: center;
+  height: 3em;
   align-items: center;
   padding-top: 0.2em;
   padding-bottom: 0.2em;
