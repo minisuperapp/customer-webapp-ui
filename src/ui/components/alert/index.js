fbe081e831d5ef35f6e6f641fd4699b6cb284c56
Raul T <jr.torsa@gmail.com>
2020-08-19 10:27:00 -0600

    created alert comppnent

---
+++ src/ui/components/alert/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { hide_alert_message } from '../../../state/actions/alert_actions'
+import Style from './style'
+
+class Alert extends Component {
+  hideAlert = () => {
+    const { hide_alert_message } = this.props
+    hide_alert_message()
+  }
+  render() {
+    const {
+      alert: { message },
+    } = this.props
+    if (message) {
+      return (
+        <Style>
+          <div className="alert-box">
+            <button className="close" onClick={this.hideAlert}>
+              X
+            </button>
+            <br />
+            {message}
+          </div>
+        </Style>
+      )
+    }
+    return null
+  }
+}
+
+function mapStateToProps(state) {
+  const { alert } = state
+  return {
+    alert,
+  }
+}
+
+const mapDispatchToProps = {
+  hide_alert_message,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Alert)
