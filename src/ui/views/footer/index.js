3bf1846f4d4de909ed400ff489691513331e81e6
alayor <alayor3@gmail.com>
2020-08-26 21:03:41 -0600

    use the navigation menu in the footer.

--- src/ui/views/footer/index.js
+++ src/ui/views/footer/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 import Style from './style'
-import { paths } from 'src/constants'
 import { search_product_request } from 'src/state/actions/product_actions'
+import NavigationMenu from 'src/ui/components/navigation_menu'
 
 class Footer extends Component {
   state = {
@@ -27,21 +26,8 @@ class Footer extends Component {
     const { active_page } = this.state
     return (
       <Style>
-        <div className="links">
-          <Link
-            className={`${
-              ![paths.register, paths.orders_list].includes(active_page) ? 'active' : ''
-            } products_link`}
-            onClick={this.refresh}
-            to={paths.home}>
-            Productos
-          </Link>
-          <Link
-            className={`${active_page === paths.orders_list ? 'active' : ''} orders_link`}
-            onClick={this.refresh}
-            to={paths.orders_list}>
-            Mis Ordenes
-          </Link>
+        <div className="navigation_menu">
+          <NavigationMenu active_page={active_page} />
         </div>
       </Style>
     )
--- src/ui/views/footer/style.js
+++ src/ui/views/footer/style.js
@@ -11,41 +11,7 @@ export default styled.div`
   background: white;
   z-index: 3;
 
-  & .links {
-    margin-top: 1em;
-    margin-bottom: 1em;
-
-    & a {
-      text-decoration: none;
-      color: black;
-    }
-
-    & .products_link {
-      border: 1px solid black;
-      border-right: none;
-      border-top-left-radius: 10px;
-      border-bottom-left-radius: 10px;
-      padding: 0.5em;
-    }
-
-    & .orders_link {
-      border: 1px solid black;
-      border-top-right-radius: 10px;
-      border-bottom-right-radius: 10px;
-      padding: 0.5em;
-    }
-
-    & .account_link {
-      border: 1px solid black;
-      border-top-right-radius: 10px;
-      border-bottom-right-radius: 10px;
-      padding: 0.5em;
-    }
-
-    & .active {
-      background-color: #003049;
-      color: white;
-    }
+  .navigation_menu {
     @media only screen and (min-width: 871px) {
       display: none;
     }
