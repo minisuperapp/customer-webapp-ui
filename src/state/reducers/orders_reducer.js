459ecb4eaf0b326f75d7f633c0a12d977ecc9781
alayor <alayor3@gmail.com>
2020-08-31 07:08:26 -0600

    Remove place order listener on orders reducer.

--- src/state/reducers/orders_reducer.js
+++ src/state/reducers/orders_reducer.js
@@ -6,10 +6,6 @@ export default function reducer(state = initial_state.orders, action) {
   switch (action.type) {
     case types.GET_CURRENT_ORDERS_RESPONSE:
       return Immutable(action.response)
-    case types.PLACE_ORDER_RESPONSE: {
-      const { orders } = action
-      return Immutable({...state, orders})
-    }
     default:
       return state
   }
