6a4fde01aa5af965608a67145037cbfc2d741619
alayor <alayor3@gmail.com>
2020-09-11 16:50:14 -0600

    change API_HOST

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -32,7 +32,7 @@ module.exports = {
     new webpack.DefinePlugin({
       // This global makes sure React is built in prod mode.
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.API_HOST': JSON.stringify('https://www.minisuper.app/comprar/api'),
+      'process.env.API_HOST': JSON.stringify('https://www.minisuper.app/customer_api'),
       'process.env.SOCKET_HOST': JSON.stringify('https://www.minisuper.app'),
       'process.env.SUBDIR': JSON.stringify('/comprar'),
     }),
