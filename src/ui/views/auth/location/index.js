a6ad1e1923b5ac0b3979cad24cca4cd283534aff
alayor <alayor3@gmail.com>
2020-09-21 12:44:29 -0600

    Select the area.

--- src/ui/views/auth/location/index.js
+++ src/ui/views/auth/location/index.js
@@ -3,50 +3,87 @@ import Style from './style'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import * as postal_areas from 'src/state/services/postal_areas'
 import logo from 'src/ui/images/logo.png'
+import Select from 'react-select'
 
 class LocationView extends Component {
   state = {
-    default_state: 'Chihuahua',
-    default_county: 'Delicias',
+    loading: false,
+    states: [
+      {
+        value: 'Chihuahua',
+        label: 'Chihuahua',
+      },
+    ],
+    counties: [
+      {
+        value: 'Delicias',
+        label: 'Delicias',
+      },
+      {
+        value: 'Meoqui',
+        label: 'Meoqui',
+      },
+      {
+        value: 'Camargo',
+        label: 'Camargo',
+      },
+    ],
     areas: [],
+    selected_area: null,
   }
 
   async componentDidMount() {
-    await this.get_postal_areas(this.state.default_county)
+    await this.get_postal_areas(this.state.counties[0].value)
   }
 
-  on_city_change = async event => {
-    await this.get_postal_areas(event.target.value)
+  on_city_change = async city => {
+    await this.get_postal_areas(city.value)
   }
 
-  get_postal_areas = async county => {
-    const { areas } = await postal_areas.get_by_state_county({ state: 'Chihuahua', county })
+  on_area_change = area => {
     this.setState({
-      areas,
+      selected_area: area,
     })
   }
 
+  get_postal_areas = async county => {
+    this.setState({ loading: true, selected_area: null })
+    const { areas } = await postal_areas.get_by_state_county({ state: 'Chihuahua', county })
+    const areas_to_select = areas.map(area => ({ value: area.code, label: area.name }))
+    this.setState(
+      {
+        areas: areas_to_select,
+      },
+      () => {
+        this.setState({ loading: false })
+      },
+    )
+  }
+
   render() {
-    const { default_county, default_state, areas } = this.state
+    const { counties, states, areas, loading, selected_area } = this.state
     return (
       <Style>
         <LazyLoadImage className="logo" alt="minisuper" src={logo} align="absmiddle" />
         <div className="welcome">Bienvenido a Minisuper</div>
         <div className="title">Antes de comenzar, selecciona tu ubicación</div>
-        <select className="state" disabled={true}>
-          <option>{default_state}</option>
-        </select>
-        <select className="city" onChange={this.on_city_change}>
-          <option>{default_county}</option>
-          <option>Meoqui</option>
-        </select>
-        <select className="area">
-          {areas.map((area, i) => (
-            <option key={i} value={area.code}>
-              {area.name}
-            </option>
-          ))}
-        </select>
+        <Select className="state" defaultValue={states[0]} options={states} />
+        <Select
+          className="city"
+          defaultValue={counties[0]}
+          options={counties}
+          onChange={this.on_city_change}
+        />
+        <Select
+          value={selected_area}
+          onChange={this.on_area_change}
+          className="area"
+          autoFocus={true}
+          options={areas}
+          placeholder="Colonia"
+          isClearable={true}
+          isLoading={loading}
+        />
       </Style>
     )
   }
