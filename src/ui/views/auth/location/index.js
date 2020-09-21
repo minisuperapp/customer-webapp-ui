aae2c37e590d38c46f52acaadb2dba1d9d9b87bf
alayor <alayor3@gmail.com>
2020-09-20 23:14:56 -0600

    Add message an title to location view.

--- src/ui/views/auth/location/index.js
+++ src/ui/views/auth/location/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Style from './style'
 
 class LocationView extends Component {
   render() {
-    return <div>
-
-    </div>
+    return (
+      <Style>
+        <div className="welcome">Bienvenido a Minisuper</div>
+        <div className="instructions">Selecciona tu ubicación</div>
+      </Style>
+    )
   }
 }
 
---
+++ src/ui/views/auth/location/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .welcome {
+    margin-top: 5em;
+    font-size: 1.3rem;
+    color: #fefefe;
+  }
+
+  .instructions {
+    margin-top: 1em;
+    font-size: 1.3rem;
+    color: #fefefe;
+  }
+`
