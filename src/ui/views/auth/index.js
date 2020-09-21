6b9d8f53419d6b267b25ad60baeea633885bfb89
alayor <alayor3@gmail.com>
2020-09-20 23:02:32 -0600

    Remove generic background color.

--- src/ui/views/auth/index.js
+++ src/ui/views/auth/index.js
@@ -3,13 +3,16 @@ import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { paths } from 'src/constants'
 import LocationView from './location'
+import Style from './style'
 
 class Auth extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path={paths.location} component={LocationView} />
-      </Switch>
+      <Style>
+        <Switch>
+          <Route exact path={paths.location} component={LocationView} />
+        </Switch>
+      </Style>
     )
   }
 }
