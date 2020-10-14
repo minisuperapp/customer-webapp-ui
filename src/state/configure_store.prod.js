5330dc1fe438ac040e49f6d62ed737766d71796f
alayor <alayor3@gmail.com>
2020-10-14 16:41:35 -0600

    Integrate logrocket and redux.

--- src/state/configure_store.prod.js
+++ src/state/configure_store.prod.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore, compose } from 'redux'
+import LogRocket from 'logrocket'
 import createRootReducer from './reducers'
 import createSagaMiddleware from 'redux-saga'
 import { createBrowserHistory } from 'history'
@@ -18,6 +19,7 @@ export default function configure_store(initial_state) {
         routerMiddleware(history),
         sagaMiddleware,
         reduxImmutableStateInvariant(), //function that warn us if we accidentally mutate any state in the store.
+        LogRocket.reduxMiddleware(),
       ),
     ),
   )
